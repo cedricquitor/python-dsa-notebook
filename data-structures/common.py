6865670f286f92625bbe268290d0c8f6fd773ddd
cedricquitor <cedricquitor.dev@gmail.com>
2023-04-08 05:02:17 +0800

    Initial commit

---
+++ data-structures/common.py
@@ -0,0 +1,11 @@
+# String
+x_string = "leetcode"
+print(x_string[0])
+
+# List
+x_list = ["leetcode", "dsa", "algorithm"]
+print(x_list[0])
+
+# Tuple
+x_tuple = ("Cedric", "John", "Dela Pasion")
+print(x_tuple[0])
